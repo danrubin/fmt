01c8ba1ec44c54ee5995efd7c370f628a3386b4c
Bryan Veloso <bryan@revyver.com>
2008-08-23 17:29:28 -0700

    Forgot the forms and views.

---
+++ applications/complaints/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from models import Complaint
+
+
+class ComplaintForm(forms.ModelForm):
+    class Meta:
+        model = Complaint
+        fields = ('name', 'complaint', 'technology')
---
+++ applications/complaints/views.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
+from django.forms.util import ErrorDict
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext, Context, Template
+from django.utils.encoding import smart_unicode, force_unicode
+from django.utils.http import urlquote
+from django.utils.safestring import mark_safe
+from django.views.generic import date_based
+
+from forms import ComplaintForm
+from models import Complaint
+
+
+def index(request):
+    """
+    A wrapper around ``django.views.generic.date_based.archive_index`` that
+    will display an entry form for users to input ``Complaint`` objects.
+    """
+    
+    complaints = Complaint.objects.all()
+    
+    if request.POST:
+        form = ComplaintForm(request.POST)
+    if form.is_valid():
+        form.save()
+    
+    return date_based.archive_index(
+        queryset=complaint,
+        date_field='published',
+        template_name='complaints/complaint_archive.html',
+        allow_empty=True,
+    )
